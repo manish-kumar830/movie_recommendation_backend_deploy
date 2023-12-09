3e1e7aecd6eadaba19134ceb9f5799dd0dd564ad
Manish Kumar <67259905+manish-kumar830@users.noreply.github.com>
2023-12-09 07:26:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ app.add_middleware(
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 MODEL_DIR = os.path.join(BASE_DIR,'model')
 
-df_url  = "https://69c3-117-220-163-42.ngrok-free.app/model/movies_dataframe.pkl"
-model_url  = "https://69c3-117-220-163-42.ngrok-free.app/model/movies_similarity_model.pkl"
+df_url  = "https://model98989.000webhostapp.com/model/movies_dataframe.pkl"
+model_url  = "https://model98989.000webhostapp.com/model/movies_similarity_model.pkl"
 
 
 with open(os.path.join(MODEL_DIR,'movies_dataframe.pkl'),'wb') as f:
